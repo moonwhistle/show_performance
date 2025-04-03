4b579dcf91de20016c8dfe267a0c8df604b06867
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-04-03 13:36:26 +0900

    feat: 프로젝트 빌드 및 .gitignore 설정

---
+++ showPerformance/src/main/java/com/example/showperformance/ShowPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.showperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowPerformanceApplication.class, args);
+    }
+
+}
