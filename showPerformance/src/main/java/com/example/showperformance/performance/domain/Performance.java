15a78c335841455ecea84f2f16b29c1ef483a617
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-04-22 15:53:18 +0900

    feat: redis 를 사용해 cache 기능 구현

--- showPerformance/src/main/java/com/example/showperformance/performance/domain/Performance.java
+++ showPerformance/src/main/java/com/example/showperformance/performance/domain/Performance.java
@@ -1,12 +1,15 @@
 package com.example.showperformance.performance.domain;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Performance {
     private String performId;
     private String performName;
