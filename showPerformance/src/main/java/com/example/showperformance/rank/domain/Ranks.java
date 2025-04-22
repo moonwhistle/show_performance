40d4c9d44c8a4a4e35aa19ebf643361efb5395ec
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-04-22 21:16:25 +0900

    feat: RankController 구현

---
+++ showPerformance/src/main/java/com/example/showperformance/rank/domain/Ranks.java
@@ -0,0 +1,12 @@
+package com.example.showperformance.rank.domain;
+
+import java.util.List;
+
+public class Ranks {
+
+    public final List<Rank> ranks;
+
+    public Ranks(List<Rank> ranks) {
+        this.ranks = ranks;
+    }
+}
--- showPerformance/src/main/java/com/example/showperformance/rank/service/RankService.java
+++ showPerformance/src/main/java/com/example/showperformance/rank/service/RankService.java
@@ -1,8 +1,7 @@
 package com.example.showperformance.rank.service;
 
-import com.example.showperformance.rank.domain.Rank;
+import com.example.showperformance.rank.domain.Ranks;
 import com.example.showperformance.rank.infrastructure.RestTemplateRankRequester;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
@@ -18,7 +17,8 @@ public class RankService {
     private String PERFORMANCE_API_KEY;
 
     @Cacheable(value = "rank")
-    public List<Rank> ranks() {
-        return rankRequester.requestRanks(PERFORMANCE_API_KEY);
+    public Ranks ranks() {
+        return new Ranks(rankRequester.requestRanks(PERFORMANCE_API_KEY));
     }
 }
+
