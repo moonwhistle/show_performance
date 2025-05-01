3ca405e32123d2e107e0d8821602e2b2f2fd656a
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-05-01 21:41:30 +0900

    feat: 구글 로그인 기능 구현

--- showPerformance/src/main/java/com/example/showperformance/member/controller/auth/GoogleAuthController.java
+++ showPerformance/src/main/java/com/example/showperformance/member/controller/auth/GoogleAuthController.java
@@ -1,9 +1,13 @@
 package com.example.showperformance.member.controller.auth;
 
+import com.example.showperformance.member.controller.auth.dto.JwtTokenResponse;
+import com.example.showperformance.member.controller.auth.dto.UrlResponse;
 import com.example.showperformance.member.service.GoogleAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +16,13 @@ public class GoogleAuthController {
 
     private final GoogleAuthService googleAuthService;
 
-    @PostMapping("/google/login")
-    public ResponseEntity<String> login() {
-        return ResponseEntity.ok(googleAuthService.getUrl());
+    @GetMapping("/google/login")
+    public ResponseEntity<UrlResponse> login() {
+        return ResponseEntity.ok(new UrlResponse(googleAuthService.getUrl()));
+    }
+
+    @PostMapping("/google/callback")
+    public ResponseEntity<JwtTokenResponse> callback(@RequestParam("code") String code) {
+        return ResponseEntity.ok(new JwtTokenResponse(googleAuthService.login(code)));
     }
 }
---
+++ showPerformance/src/main/java/com/example/showperformance/member/domain/member/Member.java
@@ -0,0 +1,27 @@
+package com.example.showperformance.member.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    public Member(String email) {
+        this.email = email;
+    }
+}
