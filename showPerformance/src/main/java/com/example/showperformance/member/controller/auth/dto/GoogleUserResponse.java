dc23b88a636088edaee43439d524308d0d2edf07
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-05-01 20:48:42 +0900

    feat: 구글 유저 정보 받아오는 client 메서드 구현

---
+++ showPerformance/src/main/java/com/example/showperformance/member/controller/auth/dto/GoogleUserResponse.java
@@ -0,0 +1,7 @@
+package com.example.showperformance.member.controller.auth.dto;
+
+public record GoogleUserResponse(
+        String name,
+        String email
+) {
+}
--- showPerformance/src/main/java/com/example/showperformance/member/service/client/GoogleOAuthClient.java
+++ showPerformance/src/main/java/com/example/showperformance/member/service/client/GoogleOAuthClient.java
@@ -1,11 +1,22 @@
 package com.example.showperformance.member.service.client;
 
+import com.example.showperformance.member.controller.auth.dto.GoogleTokenResponse;
+import com.example.showperformance.member.controller.auth.dto.GoogleUserResponse;
 import com.example.showperformance.member.domain.auth.OAuthClient;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -37,5 +48,45 @@ public class GoogleOAuthClient implements OAuthClient {
                 "&response_type=code" +
                 "&scope=" + URLEncoder.encode(scope, StandardCharsets.UTF_8) +
                 "&access_type=offline";
+    }
+
+    public GoogleUserResponse getUserInfo(String code) {
+        String token = getAccessToken(code);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // 사용자 정보를 요청
+        ResponseEntity<GoogleUserResponse> response = restTemplate.exchange(
+                USER_INFO_URL, HttpMethod.GET, entity, GoogleUserResponse.class);
+
+        return response.getBody();
+    }
+
+    private String getAccessToken(String code) {
+        String decode = URLDecoder.decode(code, StandardCharsets.UTF_8);
+        MultiValueMap<String, String> params = setTokenRequestParameter(decode);
+
+        // 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        ResponseEntity<GoogleTokenResponse> response = makeTokenRequest(request);
+        return Objects.requireNonNull(response.getBody()).access_token();
+    }
+
+    private MultiValueMap<String, String> setTokenRequestParameter(String code) {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", clientId);
+        params.add("client_secret", clientSecret);
+        params.add("redirect_uri", redirectUri);
+        params.add("code", code);
+        return params;
+    }
+
+    private ResponseEntity<GoogleTokenResponse> makeTokenRequest(HttpEntity<MultiValueMap<String, String>> request) {
+        return restTemplate.postForEntity(TOKEN_URL, request, GoogleTokenResponse.class);
     }
 }
