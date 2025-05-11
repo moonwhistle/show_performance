5f46ed388b56e20c826633a47e4c5d170bc28881
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-05-11 16:50:09 +0900

    feat: fetch 조인을 통해 favorite 데이터 한 번에 받아오는 로직 구현

--- showPerformance/src/main/java/com/example/showperformance/favorite/repository/FavoriteRepository.java
+++ showPerformance/src/main/java/com/example/showperformance/favorite/repository/FavoriteRepository.java
@@ -3,10 +3,17 @@ package com.example.showperformance.favorite.repository;
 import com.example.showperformance.favorite.domain.Favorite;
 import com.example.showperformance.member.domain.member.Member;
 import com.example.showperformance.performance.domain.Performance;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
     boolean existsByMemberAndPerformance(Member member, Performance performance);
     void deleteByMemberAndPerformance(Member member, Performance performance);
+
+    @Query("SELECT f FROM Favorite f JOIN FETCH f.performance WHERE f.member.id = :memberId")
+    Page<Favorite> findAllByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 }
