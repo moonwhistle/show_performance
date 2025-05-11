b376eaa0ff59f176c477d7e75ae514c480e8e221
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-05-11 15:56:43 +0900

    feat: argumentResolver 등록

--- showPerformance/src/main/java/com/example/showperformance/config/WebConfig.java
+++ showPerformance/src/main/java/com/example/showperformance/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.example.showperformance.config;
 
+import com.example.showperformance.global.resolver.AuthArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthArgumentResolver resolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -13,5 +20,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(resolver);
     }
 }
